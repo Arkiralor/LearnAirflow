0e6d797077d260ced8abdde6aa077904e535bcaf
Arkiralor <prithoo11335@gmail.com>
2023-03-17 00:42:21 +0530

    huh

--- dags/our_first_dag.py
+++ dags/our_first_dag.py
@@ -9,15 +9,15 @@ default_args = {
 }
 
 with DAG(
-    dag_id = "our_first_dag",
-    default_args = default_args,
-    description = "This is the first Directed Acyclic Graph that we wrote in Apache Airflow.",
-    start_date = datetime(2023, 3, 17, 0, 27, 0, 0),
-    schedule_interval = "@daily"
+    dag_id="our_first_dag",
+    default_args=default_args,
+    description="This is the first Directed Acyclic Graph that we wrote in Apache Airflow.",
+    start_date=datetime(2023, 3, 17, 0, 27, 0, 0),
+    schedule_interval="@daily"
 ) as dag:
     task_1 = BashOperator(
-        task_id = "first_task",
-        bash_command = "echo 'Hello World! This is the first task.'"
+        task_id="first_task",
+        bash_command="echo 'Hello World! This is the first task.'"
     )
 
     task_1
