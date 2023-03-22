3b2ea583cf1008a924cc562ccab84deae18edef5
Arkiralor <prithoo11335@gmail.com>
2023-03-22 14:00:46 +0530

    Xcomms

--- dags/use_pythhon_operator.py
+++ dags/use_pythhon_operator.py
@@ -6,10 +6,31 @@ from pytz import timezone
 
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from airflow.models.taskinstance import TaskInstance
+
+DAG_ID = "python_operation_v10"
+NAME = "Artoria Uthersdottir Pendragon"
 
 
-def greet() -> None:
-    print(f"[{datetime.now(tz=timezone('Asia/Kolkata'))}]\tHello, from the python DAG!")
+def greet(ti:TaskInstance, age:int=None) -> None:
+    name = ti.xcom_pull(task_ids="get_name", dag_id=DAG_ID)
+
+    resp = f"[{datetime.now(tz=timezone('Asia/Kolkata'))}]\tHello, from the python DAG!"
+    if name and not age:
+        resp = f"[{datetime.now(tz=timezone('Asia/Kolkata'))}]\tHello, from the python DAG! My name is {name}."
+    elif age and not name:
+        resp = f"[{datetime.now(tz=timezone('Asia/Kolkata'))}]\tHello, from the python DAG! I am {age} year old" if age == 1 else f"[{datetime.now(tz=timezone('Asia/Kolkata'))}]\tHello, from the python DAG! I am {age} years old."
+    elif name and age:
+        resp = f"[{datetime.now(tz=timezone('Asia/Kolkata'))}]\tHello, from the python DAG! My name is {name} and I am {age} year old" if age == 1 else f"[{datetime.now(tz=timezone('Asia/Kolkata'))}]\tHello, from the python DAG! My name is {name} and I am {age} years old."
+    else:
+        resp = f"Invalid argument(s):\n\tNAME: {name}\n\tAGE: {age}"
+    print(resp)
+
+    return True
+
+
+def get_name():
+    return NAME
 
 
 default_args = {
@@ -29,16 +50,27 @@ datetime_obj = datetime(
     tzinfo=timezone('Asia/Kolkata')
 )
 
+
+
+
 with DAG(
-    dag_id="python_operation",
+    dag_id=DAG_ID,
     default_args=default_args,
     description="This is a python operation in Apache Airflow using the basic `PythonOperator`.",
     start_date=datetime_obj,
     schedule_interval="@daily"
 ) as dag:
     task1 = PythonOperator(
-        task_id="greet",
-        python_callable=greet
+        task_id="get_name",
+        python_callable=get_name
     )
 
-    task1
+    task2 = PythonOperator(
+        task_id="greet",
+        python_callable=greet,
+        op_kwargs = {
+            "age": 30
+        }
+    )
+
+    task1 >> task2
